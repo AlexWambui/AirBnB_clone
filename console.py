216b56acbde5e8b2064d666420498a8ac15489f7
AlexWambui <alexwambuik@gmail.com>
2022-01-29 11:47:04 -0800

    update: made the file executable and added a class that prompts users with (hbnb) plus a few basic methods

--- console.py
+++ console.py
@@ -1,5 +1,45 @@
-#!usr/bin/python3
+#!/usr/bin/python3
 """
 Building a command interpreter
 """
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+	"""
+	Command interpreter for the HBNB console.
+	"""
+	prompt = "(hbnb) "
+#	intro = '''
+#	|+-----------------------------------------+|
+#	|                                           |
+#	| Welcome to the HBNB Console!              |
+#	| Enter 'help' to see a list of commands.   |
+#	|                                           |
+#	|+-----------------------------------------+|
+#	|
+#	|
+#	'''
+	
+	def do_quit(self, line):
+		"""Quit command to exit the program."""
+		raise SystemExit
+
+	def do_EOF(self, line):
+		"""Exit with Ctrl+D."""
+		return True
+
+	def do_help(self, line):
+		"""
+		Get help on commands.
+		Usage: help <command>
+		"""
+		cmd.Cmd.do_help(self, line)
+
+	def emptyline(self):
+		"""Do nothing on empty input."""
+		pass
+
+if __name__ == "__main__":
+	HBNBCommand().cmdloop()
 
